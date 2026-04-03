33a673ae84d48bee845ef850fc577e5e2660fe0c
Sergey Kozyrev <serega3306@mail.ru>
2026-04-03 14:03:06 -0400

    update

--- db/db_analytics.py
+++ db/db_analytics.py
@@ -7,10 +7,10 @@ from sqlalchemy import cast, desc, Integer, select, func
 
 from routers.schemas import Analytics
 from sqlalchemy.orm import Session
-from db.models import DbExpense, DbAccount
+from db.models import DbExpense, DbAccount, DbRevenue
 import datetime
 
-def analytics(db: Session, user_id:int, account_types:list[str]):
+def analytics_expense(db: Session, user_id:int, account_types:list[str]):
     info_list=[]
     yearly_summary = []
     current_month=str(datetime.date.today().month)
@@ -19,10 +19,7 @@ def analytics(db: Session, user_id:int, account_types:list[str]):
         "utilitiesPayment": "Utilities",
         "otherPayment": "Other Payment",
         "medicine": "Medicine",
-        "transferLineOfCredit": "Transfer to Line of Credit",
-        "transferToVisa": "Transfer to Visa",
         "grocery": "Grocery",
-        "transferToChequing": "Transfer to Chequing",
     }
 
     information = (
@@ -67,6 +64,78 @@ def analytics(db: Session, user_id:int, account_types:list[str]):
             DbExpense.transaction_month,
             DbExpense.category,
             DbExpense.transaction_year
+        )
+        .all()
+    )
+
+    for title, amount, month, year in category_summary:
+        yearly_summary.append({
+            "title": title,
+            "amount": round(amount, 2),
+            "month": month,
+            "year": year
+        })
+    for record in info_list:
+        for item in yearly_summary:
+            if record["type"] == item["title"]:
+                record.setdefault("summary", []).append(item)
+    return {'info':info_list}
+
+def analytics_revenue(db: Session, user_id:int, account_types:list[str]):
+    info_list=[]
+    yearly_summary = []
+    current_month=str(datetime.date.today().month)
+    # category_list = ['grocery', 'transferToVisa', 'utilitiesPayment', 'otherPayment', 'transferLineOfCredit', 'transferToChequing', 'medicine']
+    category_titles = {
+        "transferToChequing": "Transfer to Cheching",
+        "transferToVisa": "Transfer to Visa",
+        "transferToLineOfCredit": "Transfer to Line of Credit",
+        "salary": "Salary",
+    }
+
+    information = (
+        db.query(
+            DbRevenue.category,
+            func.sum(DbRevenue.revenue_balance),
+            DbRevenue.transaction_year
+        )
+        .filter(
+            DbRevenue.user_id == user_id,
+            DbRevenue.transaction_month == current_month,
+            DbRevenue.account_type.in_(account_types)
+        )
+        .group_by(
+            DbRevenue.transaction_month,
+            DbRevenue.category,
+            DbRevenue.transaction_year
+        )
+        .all()
+    )
+    print('information_revenue',information)
+    for category, amount, year in information:
+        info_list.append({
+            "title": category_titles.get(category, category),
+            "amount": round(amount, 2),
+            "year": year,
+            "type": category
+        })
+
+
+    category_summary = (
+        db.query(
+            DbRevenue.category,
+            func.sum(DbRevenue.revenue_balance),
+            DbRevenue.transaction_month,
+            DbRevenue.transaction_year
+        )
+        .filter(
+            DbRevenue.user_id == user_id,
+            DbRevenue.account_type.in_(account_types)
+        )
+        .group_by(
+            DbRevenue.transaction_month,
+            DbRevenue.category,
+            DbRevenue.transaction_year
         )
         .all()
     )
--- routers/analytics.py
+++ routers/analytics.py
@@ -19,6 +19,7 @@ router = APIRouter(
 @router.get('')
 def analytics_info(db: Session = Depends(get_db), current_user:UserAuth=Depends(get_current_user)):
 
-    info_chequing_line_of_credit=db_analytics.analytics(db, current_user.user_id, ['Chequing', 'LineOfCredit'])
-    info_visa=db_analytics.analytics(db, current_user.user_id, ['Visa'])
-    return{'infoChequingLineOfCredit': info_chequing_line_of_credit, 'infoVisa': info_visa}
+    info_chequing_line_of_credit=db_analytics.analytics_expense(db, current_user.user_id, ['Chequing', 'LineOfCredit'])
+    info_visa=db_analytics.analytics_expense(db, current_user.user_id, ['Visa'])
+    info_revenue=db_analytics.analytics_revenue(db, current_user.user_id, ['Chequing', 'LineOfCredit', 'Visa'])
+    return{'infoChequingLineOfCredit': info_chequing_line_of_credit, 'infoVisa': info_visa, 'infoRevenue': info_revenue}
